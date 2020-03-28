2701b0eb11855cd3f282503248a6c6deab7cd347
Deijah Wilson <deijahwilson1@gmail.com>
2020-03-28 17:49:15 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -12,12 +12,12 @@
       
             item = @@items.find{|i| i.name == item_name}
       
-            if item.nil?
-              resp.write "Item not found"
-              resp.status = 400
-            else
-              resp.write item.price
-            end
+            if item 
+                resp.write item.price
+              else
+                resp.write "Item not found"
+                resp.status = 400
+              end
           else
             resp.write "Route not found"
             resp.status = 404
